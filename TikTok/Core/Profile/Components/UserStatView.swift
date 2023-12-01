3d51914490ff570c13d6b3921908439cdf6ea0f1
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-12-01 21:33:52 +0200

    profileview added

---
+++ TikTok/Core/Profile/Components/UserStatView.swift
@@ -0,0 +1,30 @@
+//
+//  UserStatView.swift
+//  TikTok
+//
+//  Created by Y K on 01.12.2023.
+//
+
+import SwiftUI
+
+struct UserStatView: View {
+    let value: Int
+    let title: String
+    
+    var body: some View {
+        VStack {
+            Text("\(value)")
+                .font(.subheadline)
+                .fontWeight(.bold)
+            
+            Text(title)
+                .font(.caption)
+                .foregroundStyle(.gray)
+        }
+        .frame(width: 80, alignment: .center)
+    }
+}
+
+#Preview {
+    UserStatView(value: 5, title: "Following")
+}
---
+++ TikTok/Core/Profile/View/CurrentUserProfileView.swift
@@ -0,0 +1,31 @@
+//
+//  CurrentUserProfileView.swift
+//  TikTok
+//
+//  Created by Y K on 01.12.2023.
+//
+
+import SwiftUI
+
+struct CurrentUserProfileView: View {
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack(spacing: 2) {
+                    // profile header
+                    ProfileHeaderView()
+                    // post grid view
+                    PostGridView()
+                }
+                .padding(.top)
+
+            }
+            .navigationTitle("Profile")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+    }
+}
+
+#Preview {
+    CurrentUserProfileView()
+}
