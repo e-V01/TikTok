d6a6653d5d2f67d221079a02f630e21002e11186
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-12-01 17:36:29 +0200

    tabview created and logic to switch between screen also

---
+++ TikTok/Core/TabBar/MainTabView.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabView.swift
+//  TikTok
+//
+//  Created by Y K on 01.12.2023.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainTabView()
+}
