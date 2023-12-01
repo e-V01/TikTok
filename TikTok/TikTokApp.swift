7abdfccabab4a64f8a85f1214be1d101a5905b2b
e-V01 <91158407+e-V01@users.noreply.github.com>
2023-12-01 17:10:49 +0200

    Initial Commit

---
+++ TikTok/TikTokApp.swift
@@ -0,0 +1,17 @@
+//
+//  TikTokApp.swift
+//  TikTok
+//
+//  Created by Y K on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct TikTokApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
